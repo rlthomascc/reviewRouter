18634c86060cbbc54fe52ad90535e1c607282bd8
Randy <rlthomasccv@gmail.com>
2018-12-17 20:55:45 -0800

    fixes to bug

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -6,87 +6,43 @@ class Form extends Component {
     form() {
         return (
             <div id="form">
+            <h3>GIFT CARD INFORMATION HERE</h3>
                 <form>
 
                     <div className="form-group col-sm-5">
-                        <label for="full name">Full Name</label>
-                        <input type="text" className="form-control" id="full name" placeholder="John Doe" />
+                        <label for="full name"><b>Full Name</b></label>
+                        <input type="text" className="form-control" id="full name" placeholder="John Doe" required />
                     </div>
 
                     <div className="col-sm">
-                        <label for="experienceRating">How would you rate your overall experience with The Del Real Group</label>
+                        <label for="experienceRating"><b>How would you rate your overall experience with The Del Real Group</b></label>
                     </div>
                     <div className="col-sm">
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="experienceRadioOptions" id="experienceRadio1" value="1" />
+                            <input className="form-check-input" type="radio" name="experienceRadioOptions" id="experienceRadio1" value="1" required/>
                             <label className="form-check-label" for="experienceRating1">1</label>
                         </div>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="experienceRadioOptions" id="experienceRadio2" value="2" />
+                            <input className="form-check-input" type="radio" name="experienceRadioOptions" id="experienceRadio2" value="2" required/>
                             <label className="form-check-label" for="experienceRating2">2</label>
                         </div>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="experienceRadioOptions" id="experienceRadio3" value="3" />
+                            <input className="form-check-input" type="radio" name="experienceRadioOptions" id="experienceRadio3" value="3" required/>
                             <label className="form-check-label" for="experienceRating2">3</label>
                         </div>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="experienceRadioOptions" id="experienceRadio4" value="4" />
+                            <input className="form-check-input" type="radio" name="experienceRadioOptions" id="experienceRadio4" value="4" required/>
                             <label className="form-check-label" for="experienceRating2">4</label>
                         </div>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="experienceRadioOptions" id="experienceRadio5" value="5" />
+                            <input className="form-check-input" type="radio" name="experienceRadioOptions" id="experienceRadio5" value="5" required/>
                             <label className="form-check-label" for="experienceRating2">5</label>
                         </div>
                     </div>
                     <br></br>
 
                     <div className="col-sm">
-                        <label for="closingRating">Did we close your property on time? </label>
-                    </div>
-                    <div className="col-sm">
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="closingRatingOptions" id="closingRating1" value="Yes" />
-                            <label className="form-check-label" for="closingRating1">Yes</label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="closingRatingOptions" id="closingRating2" value="No" />
-                            <label className="form-check-label" for="closingRating2">No</label>
-                        </div>
-                    </div>
-                    <br></br>
-
-                    <div className="col-sm">
-                        <label for="communicationRating">Did we effectively communicate with you during the process? </label>
-                    </div>
-                    <div className="col-sm">
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="communicationRatingOptions" id="communicationRating1" value="Yes" />
-                            <label className="form-check-label" for="communicationRating1">Yes</label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="communicationRatingOptions" id="communicationRating2" value="No" />
-                            <label className="form-check-label" for="communicationRating2">No</label>
-                        </div>
-                    </div>
-                    <br></br>
-
-                    <div className="col-sm">
-                        <label for="useAgainRating">Will you use The Del Real Group in the future should you need another home? </label>
-                    </div>
-                    <div className="col-sm">
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="useAgainRating" id="useAgainRating1" value="Yes" />
-                            <label className="form-check-label" for="useAgainRating1">Yes</label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="useAgainRating" id="useAgainRating2" value="No" />
-                            <label className="form-check-label" for="useAgainRating2">No</label>
-                        </div>
-                    </div>
-                    <br></br>
-
-                    <div className="col-sm">
-                        <label for="recommendRating">Will you recommend The Del Real Group to friends and family based on your experience? </label>
+                        <label for="recommendRating"><b>Will you recommend The Del Real Group to friends and family based on your experience?</b> </label>
                     </div>
                     <div className="col-sm">
                         <div className="form-check form-check-inline">
@@ -101,29 +57,14 @@ class Form extends Component {
                     <br></br>
 
                     <div className="col-sm">
-                        <label for="contacted">Please provide your comments about your loan experience, the people you worked with at <br></br>
-                        The Del Real Group, or any suggestions you may have.</label>
-                        <textarea className="form-control col-sm" id="comments" rows="3" />
+                        <label for="contacted"><b>Please provide your comments about your loan experience, the people you worked with at <br></br>
+                        The Del Real Group, or any suggestions you may have.</b></label>
+                        <textarea className="form-control col-sm" id="comments" rows="3" required/>
                     </div>
                     <br></br>
 
                     <div className="col-sm">
-                        <label for="contacted">Would you like to be contacted regarding your feedback? </label>
-                    </div>
-                    <div className="col-sm">
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="contacted" id="contacted1" value="Yes" />
-                            <label className="form-check-label" for="contacted1">Yes</label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="contacted" id="contacted2" value="No" />
-                            <label className="form-check-label" for="contacted2">No</label>
-                        </div>
-                    </div>
-                    <br></br>
-
-                    <div className="col-sm">
-                        <label for="marketing">May we use your feedback from this survey as a testimonial for marketing purposes? </label>
+                        <label for="marketing"><b>May we use your feedback from this survey as a testimonial for marketing purposes?</b> </label>
                     </div>
                     <div className="col-sm">
                         <div className="form-check form-check-inline">
@@ -137,8 +78,16 @@ class Form extends Component {
                     </div>
                     <br></br>
 
+                    <div className="col-sm">
+                        <label for="contacted" required> <i>The Del Real Group is always looking for ways that we can improve your real estate experience.</i> <br></br>
+                        <b>What is at least one thing we can do to improve?</b> </label>
+                        <input className="form-control col-sm-5" id="comments" rows="3" />
+                    </div>
+                    <br></br>
+
                     <button className="btn btn-primary btn-lg" type="submit">Submit</button>
                 </form>
+                <p>CONTEST RULES LINK // USE MODAL FOR FORM POP UP WITH INFORMATION</p>
             </div>
         );
     }
