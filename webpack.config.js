7f3752e17294f8fdcb25175af347872e45babce0
Randy <rlthomasccv@gmail.com>
2019-01-03 12:57:39 -0800

    REFACTOR RESOLVE

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,31 @@
-// var path = require('path');
-// var SRC_DIR = path.join(__dirname, '/client/src');
-// var DIST_DIR = path.join(__dirname, '/client/dist');
-
-// module.exports = {
-//   entry: `${SRC_DIR}/index.js`,
-//   output: {
-//     filename: 'bundle.js',
-//     path: DIST_DIR
-//   },
-//   module : {
-//     rules : [
-//       {
-//         test : /\.jsx?/,
-//         include : SRC_DIR,
-//         loader : 'babel-loader',
-//         query: {
-//           presets: ['react', 'es2015']
-//         }
-//       },
-//     ]
-//   }
-// };
-
-const path = require('path');
-
-const SRC_DIR = path.join(__dirname, '/client/src');
-const DIST_DIR = path.join(__dirname, '/client/dist');
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.js`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR,
+    path: DIST_DIR
   },
-  module: {
-    loaders: [
+  module : {
+    loaders : [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        loader : 'babel-loader',
         query: {
-          presets: ['react', 'es2015'],
-        },
+          presets: ['react', 'es2015']
+        }
       },
-    ],
-  },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader",
+      },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=100000'
+    }
+    ]
+  }
 };
