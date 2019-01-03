50db7ba274258a5e2c20c79c6d116573d9cdc082
Randy <rlthomasccv@gmail.com>
2019-01-02 16:21:23 -0800

    heroku

--- server/index.js
+++ server/index.js
@@ -2,23 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('../database/index.js');
 
+
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
-let port = process.env.PORT || 3000;
-
-app.listen(port, function() {
-  console.log(`listening on port ${port}`);
-});
-
-
 
 app.post('/review', (req, res) => {
   db.save(req.body);
   res.send('data has been sent from server to database')
 });
 
+let port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
 
 
